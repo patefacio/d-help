80db2a1f2d22fba35bb06db4d883f869dbf2c344
Daniel Davidson <dbdavidson@yahoo.com>
2012-12-10 06:36:55 -0600

    broke dup out into own module

---
+++ d-help/opmix/dup.d
@@ -0,0 +1,193 @@
+/**
+   Support for generalized dup
+*/
+
+module opmix.dup;
+
+import std.algorithm;
+import std.string;
+import std.traits;
+
+/**
+   Set this to true to see what is going on at compile time
+*/
+const(bool) LogDupCompile = false;
+
+
+// custom <dmodule dup public_section>
+
+template DeepUnqual(T) {
+  static if(isAssociativeArray!T) {
+    alias Unqual!(Unqual!(ValueType!T)[Unqual!(KeyType!T)]) DeepUnqual;    
+  } else static if(isDynamicArray!T) {
+    alias Unqual!(Unqual!(ArrayElementType!T)[]) DeepUnqual;
+  } else static if(isPointer!T) {
+    alias Unqual!(PointerTarget!T) * DeepUnqual;
+  } else {
+    alias Unqual!T DeepUnqual;
+  }
+}
+
+template ArrayElementType(T : U[], U) {
+  alias U ArrayElementType;
+}
+
+template isArrayOfNonMutable(T) {
+  static if(isDynamicArray!T && 
+            (is(ArrayElementType!T == const) ||
+             is(ArrayElementType!T == immutable))) {
+    enum isArrayOfImmutable = true;
+  } else {
+    enum isArrayOfImmutable = false;
+  }
+}
+
+template isArrayOfImmutable(T) {
+  static if(isDynamicArray!T && is(ArrayElementType!T == immutable)) {
+    enum isArrayOfImmutable = true;
+  } else {
+    enum isArrayOfImmutable = false;
+  }
+}
+
+@property auto gdup(T)(const ref T t) {
+  DeepUnqual!T result;
+  static if(LogDupCompile) 
+    pragma(msg, 
+           "CT: gdup prop on (", 
+           typeof(result), " <= ", typeof(t), ")");
+  gdup(result, t);
+  return result;
+}
+
+ref T opDupPreferred(T, F)(ref T target, const ref F src)
+  if(is(DeepUnqual!T == DeepUnqual!F)) {
+  static if(LogDupCompile) 
+    pragma(msg, 
+           "CT: opDupPreferred on (", typeof(target), " <= ", typeof(src), ")");
+  static if(is(T==struct) && __traits(compiles, (src.dup))) {
+    static if(LogDupCompile) 
+      pragma(msg, 
+             "CT: ...Using ", 
+             typeof(src).stringof, ".dup for dup of ", typeof(src));
+    target = src.dup;
+  } else {
+    static if(LogDupCompile) 
+      pragma(msg, "CT: ...Using gdup for dup of ", typeof(src));
+    gdup(target, src);
+  }
+  return target;
+}
+
+void gdup(T1, T2)(ref T1 t1, const ref T2 t2) {
+  static assert(is(Unqual!(typeof(t1)) == typeof(t1)), 
+                "Must dup into mutable "~typeof(t1).stringof~
+                " <= "~typeof(t2).stringof);
+
+  static if(LogDupCompile) 
+    pragma(msg, 
+           "CT: gdup(t1,t2) prop on (", 
+           typeof(t1), " <= ", typeof(t2), ")");
+
+  static if(isBasicType!T1 || is(T1==enum)) {
+    static if(LogDupCompile) 
+      pragma(msg, "CT: ...T1 is basic type ", T1);
+    t1 = t2;
+  } else static if(isArrayOfImmutable!T1 && isArrayOfImmutable!T2) {
+    static if(LogDupCompile) 
+      pragma(msg, "CT: ...T1 is array of immutable ", T1, " as is t2 ", T2);
+    t1 = t2.idup;
+  } else static if(isDynamicArray!T1) {
+    static if(hasAliasing!(ArrayElementType!T1)) {
+      static if(LogDupCompile) 
+        pragma(msg, "CT: ...T1 is array mutable ", T1);
+      // If gdup is being called from postblit then t1 is t2 and we need
+      // to have a fresh temp to copy into
+      typeof(t1) temp;
+      bool isPostBlit = (t1 is t2);
+      typeof(t1) *bestChoice = isPostBlit? &temp : &t1;
+      (*bestChoice).clear();
+      foreach(ref val2; t2) {
+        Unqual!(ArrayElementType!T1) val1;
+        opDupPreferred(val1, val2);
+        (*bestChoice) ~= val1;
+      }
+      if(isPostBlit) {
+        swap(t1, temp);
+      }
+    } else {
+      static if(LogDupCompile) 
+        pragma(msg, "CT: ...T1 is array of type with no aliasing ", T1);
+      t1 = t2.dup;
+    }
+  } else static if(isAssociativeArray!T1) {
+    static if(LogDupCompile) 
+      pragma(msg, "CT: ...T1 is assoc array ", T1, 
+             " w value type ", ValueType!T1, " aka ", typeof(t1));
+
+    alias KeyType!(T1) AAKeyType;
+    alias ValueType!(T1) AAValueType;
+    alias DeepUnqual!(AAValueType)[DeepUnqual!(AAKeyType)] NoConstAssocArr;
+    typeof(t1) temp;
+    bool isPostBlit = ((cast(DeepUnqual!T1)t1) is (cast(DeepUnqual!T2)t2));
+    typeof(t1) *bestChoice = isPostBlit? &temp : &t1;
+    if(!isPostBlit) (*bestChoice).clear();
+    // NOTE: k here *is* copied with postblit, which is a shame. It would be
+    // better if the foreach would be like
+    // foreach(ref const(K) k, ref V v2; ...)
+    // foreach(ref const(K) k, ref const(V) v2; ...)
+    // per the user's choosing
+    foreach(k, ref v2; cast(DeepUnqual!(typeof(t2)))t2) {
+      AAValueType v1;
+      opDupPreferred(v1, v2);
+      (*bestChoice)[k] = v1;
+    }
+    if(isPostBlit) {
+      swap(t1, temp);
+    }
+    static if(LogDupCompile) pragma(msg, "CT: ...T1 done assoc array ", T1);
+  } else static if(is(T1==struct)) {
+    static if(LogDupCompile) pragma(msg, "CT: ...T1 is struct ", T1);
+      Unqual!(typeof(t1)) temp;
+      bool isPostBlit = t1 is t2;
+      typeof(t1) *bestChoice = isPostBlit? &temp : &t1;
+      foreach (i, ignore ; typeof(T1.tupleof)) {
+        static if(T1.tupleof[i].stringof.endsWith(".this")) {
+          static assert(0, "no dup of nested non static structs!");
+        }
+        static if(LogDupCompile) 
+          pragma(msg, "CT: .....dupping field ", t2.tupleof[i].stringof);
+        opDupPreferred((*bestChoice).tupleof[i], t2.tupleof[i]);
+      }
+      if(isPostBlit) {
+        swap(t1, temp);
+      }
+  } else static if(isPointer!T1) {
+    static if(LogDupCompile) pragma(msg, "CT: ...T1 is pointer ", T1);
+    alias Unqual!(PointerTarget!T1) Target;
+    if(t2) {
+      static if(__traits(compiles, (t1 = new Target))) {
+        t1 = new Target;
+        opDupPreferred(*t1, *t2); 
+      } else {
+        static assert(0, "gdup will not work with "~
+                      Target.stringof~
+                      " since can not be default heap allocated");
+      }
+    }
+  } else static if(is(T1==union)) {
+      t1 = t2;
+  } else static if(is(T1==class)) {
+    static if(LogDupCompile) 
+      pragma(msg, "CT: ...T1 is a class else entirely ", T1);
+    static assert(0, "Class gdup not supported, requested type "~T1.stringof);
+  } else {
+    static assert(0, "Missing gdup support for "~T1.stringof);
+  }
+}
+
+// end <dmodule dup public_section>
+
+/**
+   License: <a href="http://www.boost.org/LICENSE_1_0.txt">Boost License 1.0</a>.
+*/
--- d-help/opmix/mix.d
+++ d-help/opmix/mix.d
@@ -29,6 +29,7 @@
 
 module opmix.mix;
 
+public import opmix.dup;
 import std.algorithm;
 import std.math;
 import std.string;
@@ -252,123 +253,6 @@ template ReadWrite(alias name) {
   enum ReadWrite = ReadOnly!name;
 }
 
-@property auto gdup(T)(const ref T t) {
-  DeepUnqual!T result;
-  static if(LogCompile) 
-    pragma(msg, 
-           "CT: gdup prop on (", 
-           typeof(result), " <= ", typeof(t), ")");
-  gdup(result, t);
-  return result;
-}
-
-void gdup(T1, T2)(ref T1 t1, const ref T2 t2) {
-  static assert(is(Unqual!(typeof(t1)) == typeof(t1)), 
-                "Must dup into mutable "~typeof(t1).stringof~
-                " <= "~typeof(t2).stringof);
-
-  static if(LogCompile) 
-    pragma(msg, 
-           "CT: gdup(t1,t2) prop on (", 
-           typeof(t1), " <= ", typeof(t2), ")");
-
-  static if(isBasicType!T1 || is(T1==enum)) {
-    static if(LogCompile) 
-      pragma(msg, "CT: ...T1 is basic type ", T1);
-    t1 = t2;
-  } else static if(isArrayOfImmutable!T1 && isArrayOfImmutable!T2) {
-    static if(LogCompile) 
-      pragma(msg, "CT: ...T1 is array of immutable ", T1, " as is t2 ", T2);
-    t1 = t2.idup;
-  } else static if(isDynamicArray!T1) {
-    static if(hasAliasing!(ArrayElementType!T1)) {
-      static if(LogCompile) 
-        pragma(msg, "CT: ...T1 is array mutable ", T1);
-      // If gdup is being called from postblit then t1 is t2 and we need
-      // to have a fresh temp to copy into
-      typeof(t1) temp;
-      bool isPostBlit = (t1 is t2);
-      typeof(t1) *bestChoice = isPostBlit? &temp : &t1;
-      (*bestChoice).clear();
-      foreach(ref val2; t2) {
-        Unqual!(ArrayElementType!T1) val1;
-        opDupPreferred(val1, val2);
-        (*bestChoice) ~= val1;
-      }
-      if(isPostBlit) {
-        swap(t1, temp);
-      }
-    } else {
-      static if(LogCompile) 
-        pragma(msg, "CT: ...T1 is array of type with no aliasing ", T1);
-      t1 = t2.dup;
-    }
-  } else static if(isAssociativeArray!T1) {
-    static if(LogCompile) 
-      pragma(msg, "CT: ...T1 is assoc array ", T1, 
-             " w value type ", ValueType!T1, " aka ", typeof(t1));
-
-    alias KeyType!(T1) AAKeyType;
-    alias ValueType!(T1) AAValueType;
-    alias DeepUnqual!(AAValueType)[DeepUnqual!(AAKeyType)] NoConstAssocArr;
-    typeof(t1) temp;
-    bool isPostBlit = ((cast(DeepUnqual!T1)t1) is (cast(DeepUnqual!T2)t2));
-    typeof(t1) *bestChoice = isPostBlit? &temp : &t1;
-    if(!isPostBlit) (*bestChoice).clear();
-    // NOTE: k here *is* copied with postblit, which is a shame. It would be
-    // better if the foreach would be like
-    // foreach(ref const(K) k, ref V v2; ...)
-    // foreach(ref const(K) k, ref const(V) v2; ...)
-    // per the user's choosing
-    foreach(k, ref v2; cast(DeepUnqual!(typeof(t2)))t2) {
-      AAValueType v1;
-      opDupPreferred(v1, v2);
-      (*bestChoice)[k] = v1;
-    }
-    if(isPostBlit) {
-      swap(t1, temp);
-    }
-    static if(LogCompile) pragma(msg, "CT: ...T1 done assoc array ", T1);
-  } else static if(is(T1==struct)) {
-    static if(LogCompile) pragma(msg, "CT: ...T1 is struct ", T1);
-      Unqual!(typeof(t1)) temp;
-      bool isPostBlit = t1 is t2;
-      typeof(t1) *bestChoice = isPostBlit? &temp : &t1;
-      foreach (i, ignore ; typeof(T1.tupleof)) {
-        static if(T1.tupleof[i].stringof.endsWith(".this")) {
-          static assert(0, "no dup of nested non static structs!");
-        }
-        static if(LogCompile) 
-          pragma(msg, "CT: .....dupping field ", t2.tupleof[i].stringof);
-        opDupPreferred((*bestChoice).tupleof[i], t2.tupleof[i]);
-      }
-      if(isPostBlit) {
-        swap(t1, temp);
-      }
-  } else static if(isPointer!T1) {
-    static if(LogCompile) pragma(msg, "CT: ...T1 is pointer ", T1);
-    alias Unqual!(PointerTarget!T1) Target;
-    if(t2) {
-      static if(__traits(compiles, (t1 = new Target))) {
-        t1 = new Target;
-        opDupPreferred(*t1, *t2); 
-      } else {
-        static assert(0, "gdup will not work with "~
-                      Target.stringof~
-                      " since can not be default heap allocated");
-      }
-    }
-  } else static if(is(T1==union)) {
-      t1 = t2;
-  } else static if(is(T1==class)) {
-    static if(LogCompile) 
-      pragma(msg, "CT: ...T1 is a class else entirely ", T1);
-    static assert(0, "Class gdup not supported, requested type "~T1.stringof);
-  } else {
-    static assert(0, "Missing gdup support for "~T1.stringof);
-  }
-}
-
 /** Compare for equality all fields in a class
     Original courtesy of Tobias Pankrath
     Updated to use tupleof instead of getMember and short circuits
@@ -453,25 +337,6 @@ bool typesDeepEqual(T,F)(auto ref T lhs, auto ref F rhs)
     result = lhs == rhs;
   }
   return result;
-}
-
-ref T opDupPreferred(T, F)(ref T target, const ref F src)
-  if(is(DeepUnqual!T == DeepUnqual!F)) {
-  static if(LogCompile) 
-    pragma(msg, 
-           "CT: opDupPreferred on (", typeof(target), " <= ", typeof(src), ")");
-  static if(is(T==struct) && __traits(compiles, (src.dup))) {
-    static if(LogCompile) 
-      pragma(msg, 
-             "CT: ...Using ", 
-             typeof(src).stringof, ".dup for dup of ", typeof(src));
-    target = src.dup;
-  } else {
-    static if(LogCompile) 
-      pragma(msg, "CT: ...Using gdup for dup of ", typeof(src));
-    gdup(target, src);
-  }
-  return target;
 }
 
 int opCmpPreferred(T, F)(const ref T lhs, const ref F rhs) {
@@ -624,42 +489,8 @@ hash_t deepHash(T)(const ref T t) nothrow {
   return result;
 }
 
-template DeepUnqual(T) {
-  static if(isAssociativeArray!T) {
-    alias Unqual!(Unqual!(ValueType!T)[Unqual!(KeyType!T)]) DeepUnqual;    
-  } else static if(isDynamicArray!T) {
-    alias Unqual!(Unqual!(ArrayElementType!T)[]) DeepUnqual;
-  } else static if(isPointer!T) {
-    alias Unqual!(PointerTarget!T) * DeepUnqual;
-  } else {
-    alias Unqual!T DeepUnqual;
-  }
-}
-
-template ArrayElementType(T : U[], U) {
-  alias U ArrayElementType;
-}
-
 template HasDup(T) { 
   enum HasDup = __traits(hasMember, T, "dup"); 
-}
-
-template isArrayOfNonMutable(T) {
-  static if(isDynamicArray!T && 
-            (is(ArrayElementType!T == const) ||
-             is(ArrayElementType!T == immutable))) {
-    enum isArrayOfImmutable = true;
-  } else {
-    enum isArrayOfImmutable = false;
-  }
-}
-
-template isArrayOfImmutable(T) {
-  static if(isDynamicArray!T && is(ArrayElementType!T == immutable)) {
-    enum isArrayOfImmutable = true;
-  } else {
-    enum isArrayOfImmutable = false;
-  }
 }
 
 template IsImmutable(T) { 
