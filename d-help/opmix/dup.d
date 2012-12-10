2938c963e904ebd0ad2046876754f6ff896e4673
Daniel Davidson <dbdavidson@yahoo.com>
2012-12-10 08:08:42 -0600

    simplified gdup postblit case

--- d-help/opmix/dup.d
+++ d-help/opmix/dup.d
@@ -89,100 +89,85 @@ void gdup(T1, T2)(ref T1 t1, const ref T2 t2) {
            "CT: gdup(t1,t2) prop on (", 
            typeof(t1), " <= ", typeof(t2), ")");
 
-  static if(isBasicType!T1 || is(T1==enum)) {
-    static if(LogDupCompile) 
-      pragma(msg, "CT: ...T1 is basic type ", T1);
-    t1 = t2;
-  } else static if(isArrayOfImmutable!T1 && isArrayOfImmutable!T2) {
-    static if(LogDupCompile) 
-      pragma(msg, "CT: ...T1 is array of immutable ", T1, " as is t2 ", T2);
-    t1 = t2.idup;
-  } else static if(isDynamicArray!T1) {
-    static if(hasAliasing!(ArrayElementType!T1)) {
-      static if(LogDupCompile) 
-        pragma(msg, "CT: ...T1 is array mutable ", T1);
-      // If gdup is being called from postblit then t1 is t2 and we need
-      // to have a fresh temp to copy into
-      typeof(t1) temp;
-      bool isPostBlit = (t1 is t2);
-      typeof(t1) *bestChoice = isPostBlit? &temp : &t1;
-      (*bestChoice).clear();
-      foreach(ref val2; t2) {
-        Unqual!(ArrayElementType!T1) val1;
-        opDupPreferred(val1, val2);
-        (*bestChoice) ~= val1;
-      }
-      if(isPostBlit) {
-        swap(t1, temp);
-      }
-    } else {
-      static if(LogDupCompile) 
-        pragma(msg, "CT: ...T1 is array of type with no aliasing ", T1);
-      t1 = t2.dup;
-    }
-  } else static if(isAssociativeArray!T1) {
-    static if(LogDupCompile) 
-      pragma(msg, "CT: ...T1 is assoc array ", T1, 
-             " w value type ", ValueType!T1, " aka ", typeof(t1));
-
-    alias KeyType!(T1) AAKeyType;
-    alias ValueType!(T1) AAValueType;
-    alias DeepUnqual!(AAValueType)[DeepUnqual!(AAKeyType)] NoConstAssocArr;
+  if(&t1 is &t2) {
+    // Postblit may be using gdup - get fresh temp
     typeof(t1) temp;
-    bool isPostBlit = ((cast(DeepUnqual!T1)t1) is (cast(DeepUnqual!T2)t2));
-    typeof(t1) *bestChoice = isPostBlit? &temp : &t1;
-    if(!isPostBlit) (*bestChoice).clear();
-    // NOTE: k here *is* copied with postblit, which is a shame. It would be
-    // better if the foreach would be like
-    // foreach(ref const(K) k, ref V v2; ...)
-    // foreach(ref const(K) k, ref const(V) v2; ...)
-    // per the user's choosing
-    foreach(k, ref v2; cast(DeepUnqual!(typeof(t2)))t2) {
-      AAValueType v1;
-      opDupPreferred(v1, v2);
-      (*bestChoice)[k] = v1;
-    }
-    if(isPostBlit) {
-      swap(t1, temp);
-    }
-    static if(LogDupCompile) pragma(msg, "CT: ...T1 done assoc array ", T1);
-  } else static if(is(T1==struct)) {
-    static if(LogDupCompile) pragma(msg, "CT: ...T1 is struct ", T1);
-      Unqual!(typeof(t1)) temp;
-      bool isPostBlit = t1 is t2;
-      typeof(t1) *bestChoice = isPostBlit? &temp : &t1;
-      foreach (i, ignore ; typeof(T1.tupleof)) {
-        static if(T1.tupleof[i].stringof.endsWith(".this")) {
-          static assert(0, "no dup of nested non static structs!");
-        }
-        static if(LogDupCompile) 
-          pragma(msg, "CT: .....dupping field ", t2.tupleof[i].stringof);
-        opDupPreferred((*bestChoice).tupleof[i], t2.tupleof[i]);
-      }
-      if(isPostBlit) {
-        swap(t1, temp);
-      }
-  } else static if(isPointer!T1) {
-    static if(LogDupCompile) pragma(msg, "CT: ...T1 is pointer ", T1);
-    alias Unqual!(PointerTarget!T1) Target;
-    if(t2) {
-      static if(__traits(compiles, (t1 = new Target))) {
-        t1 = new Target;
-        opDupPreferred(*t1, *t2); 
-      } else {
-        static assert(0, "gdup will not work with "~
-                      Target.stringof~
-                      " since can not be default heap allocated");
-      }
-    }
-  } else static if(is(T1==union)) {
-      t1 = t2;
-  } else static if(is(T1==class)) {
-    static if(LogDupCompile) 
-      pragma(msg, "CT: ...T1 is a class else entirely ", T1);
-    static assert(0, "Class gdup not supported, requested type "~T1.stringof);
+    gdup(temp, t2);
+    swap(t1, temp);
   } else {
-    static assert(0, "Missing gdup support for "~T1.stringof);
+    static if(isBasicType!T1 || is(T1==enum)) {
+      static if(LogDupCompile) 
+        pragma(msg, "CT: ...T1 is basic type ", T1);
+      t1 = t2;
+    } else static if(isArrayOfImmutable!T1 && isArrayOfImmutable!T2) {
+      static if(LogDupCompile) 
+        pragma(msg, "CT: ...T1 is array of immutable ", T1, " as is t2 ", T2);
+      t1 = t2.idup;
+    } else static if(isDynamicArray!T1) {
+      static if(hasAliasing!(ArrayElementType!T1)) {
+        static if(LogDupCompile) 
+          pragma(msg, "CT: ...T1 is array mutable ", T1);
+        foreach(ref val2; t2) {
+          Unqual!(ArrayElementType!T1) val1;
+          opDupPreferred(val1, val2);
+          t1 ~= val1;
+        }
+      } else {
+        static if(LogDupCompile) 
+          pragma(msg, "CT: ...T1 is array of type with no aliasing ", T1);
+        t1 = t2.dup;
+      }
+    } else static if(isAssociativeArray!T1) {
+      static if(LogDupCompile) 
+        pragma(msg, "CT: ...T1 is assoc array ", T1, 
+               " w value type ", ValueType!T1, " aka ", typeof(t1));
+  
+      alias KeyType!(T1) AAKeyType;
+      alias ValueType!(T1) AAValueType;
+      alias DeepUnqual!(AAValueType)[DeepUnqual!(AAKeyType)] NoConstAssocArr;
+      // NOTE: k here *is* copied with postblit, which is a shame. It would be
+      // better if the foreach would be like
+      // foreach(ref const(K) k, ref V v2; ...)
+      // foreach(ref const(K) k, ref const(V) v2; ...)
+      // per the user's choosing
+      foreach(k, ref v2; cast(DeepUnqual!(typeof(t2)))t2) {
+        AAValueType v1;
+        opDupPreferred(v1, v2);
+        t1[k] = v1;
+      }
+      static if(LogDupCompile) pragma(msg, "CT: ...T1 done assoc array ", T1);
+    } else static if(is(T1==struct)) {
+      static if(LogDupCompile) pragma(msg, "CT: ...T1 is struct ", T1);
+        foreach (i, ignore ; typeof(T1.tupleof)) {
+          static if(T1.tupleof[i].stringof.endsWith(".this")) {
+            static assert(0, "no dup of nested non static structs!");
+          }
+          static if(LogDupCompile) 
+            pragma(msg, "CT: .....dupping field ", t2.tupleof[i].stringof);
+          opDupPreferred(t1.tupleof[i], t2.tupleof[i]);
+        }
+    } else static if(isPointer!T1) {
+      static if(LogDupCompile) pragma(msg, "CT: ...T1 is pointer ", T1);
+      alias Unqual!(PointerTarget!T1) Target;
+      if(t2) {
+        static if(__traits(compiles, (t1 = new Target))) {
+          t1 = new Target;
+          opDupPreferred(*t1, *t2); 
+        } else {
+          static assert(0, "gdup will not work with "~
+                        Target.stringof~
+                        " since can not be default heap allocated");
+        }
+      }
+    } else static if(is(T1==union)) {
+        t1 = t2;
+    } else static if(is(T1==class)) {
+      static if(LogDupCompile) 
+        pragma(msg, "CT: ...T1 is a class else entirely ", T1);
+      static assert(0, "Class gdup not supported, requested type "~T1.stringof);
+    } else {
+      static assert(0, "Missing gdup support for "~T1.stringof);
+    }
   }
 }
 
